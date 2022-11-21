f20e9904416d8dc0a9333cd935b099ca64478883
Li Kui <90845831+likui628@users.noreply.github.com>
2022-11-21 21:07:42 +0800

    init async-with-composition-api

---
+++ async-with-composition-api/README.md
@@ -0,0 +1,4 @@
+# Async with Composition API
+
+- [Async with Composition API](https://antfu.me/posts)
+- [Composable Vue](https://antfu.me/posts/composable-vue-vueday-2021)
---
+++ async-with-composition-api/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import ComponentA from "./components/ComponentA.vue";
+import ComponentB from "./components/ComponentB.vue";
+
+const i = ref(0);
+
+const onSwitch = () => {
+  i.value = (i.value + 1) % 2;
+};
+</script>
+
+<template>
+  <div>
+    <ComponentA v-if="i === 0" />
+    <ComponentB v-else />
+    <button @click="onSwitch">Switch</button>
+  </div>
+</template>
