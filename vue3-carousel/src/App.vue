643d18e0f5565385aa66f58dd4cc07bc0a7d6fb5
Li Kui <90845831+likui628@users.noreply.github.com>
2022-12-27 20:10:11 +0800

    vue3-carousel

---
+++ vue3-carousel/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Carousel from "./components/Carousel.vue";
+</script>
+
+<template>
+  <div>
+    <div>
+      <h1>vue3-carousel demo</h1>
+      <div>https://github.com/ismail9k/</div>
+    </div>
+    <Carousel />
+  </div>
+</template>
---
+++ vue3-carousel/src/components/Carousel.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { Carousel, Slide } from "vue3-carousel";
+
+import "vue3-carousel/dist/carousel.css";
+</script>
+
+<template>
+  <Carousel
+    :items-to-show="4.5"
+    :wrap-around="true"
+    :autoplay="2000"
+    snapAlign="start"
+  >
+    <Slide v-for="slide in 10" :key="slide">
+      <div class="carousel__item">{{ slide }}</div>
+    </Slide>
+  </Carousel>
+</template>
+
+<style scoped>
+.carousel__item {
+  min-height: 250px;
+  width: 100%;
+  background-color: #42b883;
+  color: #fff;
+  font-size: 20px;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.carousel__slide {
+  padding: 10px;
+}
+</style>
