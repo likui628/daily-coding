cb4e92cc37839879a92b1114177877d2e3505db2
Li Kui <90845831+likui628@users.noreply.github.com>
2022-12-29 20:59:27 +0800

    vueuse-best-practices

---
+++ vueuse-best-practices/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { useToggle } from "@vueuse/core";
+
+const initValue = ref(true);
+const toggle = useToggle(initValue);
+</script>
+
+<template>
+  <div class="box">
+    <p>Value: {{ initValue ? "ON" : "OFF" }}</p>
+    <button @click="toggle()">Toggle</button>
+    <button @click="initValue = true">Set ON</button>
+    <button @click="initValue = false">Set OFF</button>
+  </div>
+</template>
+
+<style scoped>
+.box {
+  padding: 10px;
+  border: 1px solid #eee;
+  border-radius: 5px;
+}
+
+button {
+  margin: 3px;
+}
+</style>
